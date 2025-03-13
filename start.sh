8dabd2da4faa0eb8e4805cbf15aec5295125f017
andry360 <andry360@hotmail.it>
2025-03-13 20:41:54 +0100

    Aggiunto controllo sul download e corretta url script

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 # Il comando per lanciare questo script da github è: bash -c "$(wget -qLO - https://raw.githubusercontent.com/andry360/prxmx-config/refs/heads/main/start.sh)"
 echo "Script progetto proxmox v1"
 
-GITHUB_URL= "https://raw.githubusercontent.com/andry360/prxmx-post-script/refs/heads/main"
+GITHUB_URL="https://raw.githubusercontent.com/andry360/prxmx-post-script/refs/heads/main"
 
 # Elenco degli script
 scripts=(
@@ -24,11 +24,22 @@ read -p "Inserisci il numero dello script da eseguire: " choice
 # Controlla se la scelta è valida
 if [[ "$choice" =~ ^[0-9]+$ ]] && [[ "$choice" -ge 1 && "$choice" -le ${#scripts[@]} ]]; then
     # Seleziona l'URL dello script scelto
-    script_url="${scripts[$((choice-1))]%% *}"
+    script_url="${scripts[$((choice-1))]%|*}"
     script_description="${scripts[$((choice-1))]#*|}"
+
     echo "Eseguendo lo script: $script_description"
-    # Esegui lo script selezionato
-    bash -c "$(wget -qLO - $script_url)"
+
+    # Scarica il contenuto dello script
+    script_content=$(wget -qO- "$script_url")
+
+    # Controlla se il download è avvenuto con successo e lo esegue
+    if [[ -n "$script_content" ]]; then
+        echo "Scaricato con successo. Esecuzione..."
+        bash -c "$script_content"
+    else
+        echo "❌ Errore: impossibile scaricare lo script da $script_url"
+        exit 1
+    fi
 else
     echo "Scelta non valida. Uscita."
     exit 1
