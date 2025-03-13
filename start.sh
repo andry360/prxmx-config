25a3f828e7dadd090d69898e1b7be8bed331230b
andry360 <andry360@hotmail.it>
2025-03-13 20:45:49 +0100

    inserito path corretto github

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 # Il comando per lanciare questo script da github è: bash -c "$(wget -qLO - https://raw.githubusercontent.com/andry360/prxmx-config/refs/heads/main/start.sh)"
 echo "Script progetto proxmox v1"
 
-GITHUB_URL="https://raw.githubusercontent.com/andry360/prxmx-post-script/refs/heads/main"
+GITHUB_URL="https://raw.githubusercontent.com/andry360/prxmx-config/refs/heads/main"
 
 # Elenco degli script
 scripts=(
